d487e387fe0232029116fb7174120155b7d7bcd9
rjviercech <eronald2009@gmail.com>
2022-04-29 20:27:50 -0500

    Agregamos archivos staticos

---
+++ portfolio/config/local.py
@@ -0,0 +1,23 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+
+# Database
+# https://docs.djangoproject.com/en/4.0/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
+STATIC_URL       =                        '/static/'
+STATICFILES_DIRS = (BASE_DIR,             'static'     )
+STATIC_ROOT      =  os.path.join(BASE_DIR, 'staticfiles')
+MEDIA_URL        =                        '/media/'
+MEDIA_ROOT       = os.path.join(BASE_DIR, 'media'      )
--- portfolio/urls.py
+++ portfolio/urls.py
@@ -13,9 +13,21 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path, re_path
+from django.views.static import serve
+
+from apps.blog.views import Indexview
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', Indexview.as_view(), name='index'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += [
+        re_path(r'^media/(?P<path>.*)$', serve , {
+            'document_root':settings.MEDIA_ROOT
+        })
+    ]
