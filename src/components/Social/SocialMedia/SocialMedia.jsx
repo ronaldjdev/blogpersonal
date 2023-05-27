864314c415e8f2e44bf61991e85555a5cd1bb246
ronaldjdev <eronald2009@gmail.com>
2023-05-26 23:58:38 -0500

    Feat: Add link social media

--- src/components/Social/SocialMedia/SocialMedia.jsx
+++ src/components/Social/SocialMedia/SocialMedia.jsx
@@ -1,5 +1,6 @@
-import { FaGithubAlt, FaLinkedin } from "react-icons/fa"
+import { FaGithubAlt, FaLinkedin,FaWhatsapp } from "react-icons/fa"
 import { SiGmail } from "react-icons/si"
+import { RiWhatsappFill } from "react-icons/ri"
 
 export const SocialMedia = () => {
   return (
@@ -8,16 +9,26 @@ export const SocialMedia = () => {
       <div>
         <div className="flex gap-5">
           <div className="flex">
-            <FaGithubAlt className="hover:fill-[#FFB6C1]" />
+            <a href="http://github.com/ronaldjdev" target="_blank" rel="noopener noreferrer">
+
+              <FaGithubAlt className="hover:fill-[#FFB6C1]" />
+            </a>
           </div>
           <div className="flex">
+            <a href="http://linkedin.com/in/ronaldjdev" target="_blank" rel="noopener noreferrer">
+
             <FaLinkedin />
+            </a>
           </div>
           <div className="flex" title="ronaldjdevfs@gmail.com" >
+            <a href="mailto:eronald2009@gmail.com">
+
             <SiGmail />
+            </a>
           </div>
           <div className="flex">
-            <SiGmail />
+            
+            <RiWhatsappFill  />
           </div>
         </div>
       </div>
