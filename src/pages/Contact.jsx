1d4c7503a81bf053c7b3baee034b4e4429acf9a8
ronaldjdev <eronald2009@gmail.com>
2023-05-28 05:34:50 -0500

    Feat: Add content in links navbar

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,9 +1,27 @@
-import { AboutMe, Hero, Title } from 'components'
+import {
+  ContactForm,
+  Container,
+  Title,
+} from 'components'
+import ring from 'assets/images/ring.png'
 
 export const Contact = () => {
   return (
     <>
-      <div></div>
+      <Container>
+        <div className="relative mb-40 flex flex-col items-center">
+          <img className="w-[800px]" src={ring} />
+          <div className="absolute inset-60 mt-20 mb-40">
+            <Title
+              title="El siguiente paso es tuyo"
+              subtitle="Estoy ansioso por escuchar sobre tu nuevo proyecto."
+            />
+          </div>
+        </div>
+        <div>
+          <ContactForm />
+        </div>
+      </Container>
     </>
   )
 }
