16b5dfc6e76b9308cd553565cdbecfdf6cdf26e7
ronaldjdev <eronald2009@gmail.com>
2023-05-31 11:59:42 -0500

    Fix: Image backgroud

--- src/components/Layout/Layout/Layout.jsx
+++ src/components/Layout/Layout/Layout.jsx
@@ -1,11 +1,13 @@
+// @ts-nocheck
 import React from 'react'
 import { Navbar, Footer } from 'components'
 import { Outlet } from 'react-router-dom'
+import stars2 from "assets/images/stars2.png"
 
 export const Layout = () => {
   return (
     <>
-      <div className="flex flex-col w-full min-h-screen">
+      <div className="flex flex-col w-full min-h-screen" style={{ backgroundImage: `url(${stars2})`, backgroundColor:" #030014 ", backgroundPosition:"50%" }}>
         <header>
           <Navbar />
         </header>
