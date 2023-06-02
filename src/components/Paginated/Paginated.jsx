c14a16a9948b881bf42b1a05f25a47bd040cecbb
ronaldjdev <eronald2009@gmail.com>
2023-06-02 04:50:02 -0500

    Fix: select dots in paginated

--- src/components/Paginated/Paginated.jsx
+++ src/components/Paginated/Paginated.jsx
@@ -1,32 +1,42 @@
-import React from "react"
+import React, { useState } from "react"
 import { TfiArrowRight } from 'react-icons/tfi'
 import { TfiArrowLeft } from 'react-icons/tfi'
 
 export const Paginated = () => {
+  const [selectedNumber, setSelectedNumber] = useState(null);
   const data = [
     "1", "2", "3"
-  ]
+  ];
+
+  const handleClick = (number) => {
+    setSelectedNumber(number);
+  };
+
   return (
     <>
       <div className="flex items-center gap-x-5 justify-center">
-        <li className="flex bg-button  items-center justify-center h-10 w-10 transition-all ease-in-out duration-100 relative rounded-full border p-4">
+        <li className="flex bg-button items-center justify-center h-10 w-10 transition-all ease-in-out duration-100 relative rounded-full border p-4">
           <span className="absolute flex items-center justify-center inset-0">
-            <TfiArrowLeft/>
+            <TfiArrowLeft />
           </span>
         </li>
         {data.map((counter, index) => (
-          <li key={index} className="flex  bg-button items-center justify-canter h-10 w-10 transition-all ease-in-out duration-100 relative rounded-full border p-4">
-            <span className="absolute flex items-center justify-center inset-0">
+          <li
+            key={index}
+            onClick={() => handleClick(counter)}
+            className={`flex font-inter bg-button items-center justify-center h-10 w-10 transition-all ease-in-out duration-100 relative rounded-full p-4 ${selectedNumber === counter ? 'bg-white text-rich-black font-bold' : ''}`}
+          >
+            <span className="absolute flex text-inherit items-center justify-center inset-0">
               {counter}
             </span>
           </li>
         ))}
-      <li className="flex  bg-button items-center justify-center h-10 w-10 transition-all ease-in-out duration-100 relative rounded-full border p-4">
+        <li className="flex bg-button items-center justify-center h-10 w-10 transition-all ease-in-out duration-100 relative rounded-full border p-4">
           <span className="absolute flex items-center justify-center inset-0">
-            <TfiArrowRight/>
-        </span>
-      </li>
+            <TfiArrowRight />
+          </span>
+        </li>
       </div>
     </>
-  )
+  );
 }
