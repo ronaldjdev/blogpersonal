4446009b0fc3590339f7574ffb3ec08d6a1c3166
ronaldjdev <eronald2009@gmail.com>
2023-11-25 07:08:57 -0500

    fix: Ajuste de velocidad y tamaño de particulas

--- src/components/Particles/Particles.tsx
+++ src/components/Particles/Particles.tsx
@@ -1,4 +1,4 @@
-import {useEffect, useRef } from 'react'
+import { useEffect, useRef } from 'react'
 interface Particle {
   angle: number;
   radius: number;
@@ -23,17 +23,17 @@ const ParticleCanvas: React.FC = () => {
     ctxRef.current = ctx;
 
     // Set canvas dimensions
-    canvas.width = 400;
-    canvas.height = 400;
+    canvas.width = 700;
+    canvas.height = 700;
 
     const createParticles = () => {
       particlesRef.current.push({
         angle: Math.random() * Math.PI * 2,
         radius: Math.random() * 200 + 100,
-        size: 2,
+        size: 1,
         opacity: 1,
-        speed: 0.01,
-        centerShrinkSpeed: 0.2,
+        speed: 0.005,
+        centerShrinkSpeed: 0.05
       });
     };
 
@@ -49,26 +49,26 @@ const ParticleCanvas: React.FC = () => {
         const x = canvas.width / 2 + particle.radius * Math.cos(particle.angle);
         const y = canvas.height / 2 + particle.radius * Math.sin(particle.angle);
 
-        if (particle.size > 0.1) {
-          particle.size -= 0.02;
-          particle.opacity -= 0.01;
+        if (particle.size > 0.05) {
+          particle.size -= 0.01;
+          particle.opacity -= 0.005;
         }
 
         ctx.fillStyle = `rgba(255, 255, 255, ${particle.opacity})`;
         ctx.strokeStyle = `rgba(255, 255, 255, ${particle.opacity})`;
-        ctx.lineWidth = 1;
+        ctx.lineWidth = 0.05;
         ctx.beginPath();
         ctx.arc(x, y, particle.size, 0, Math.PI * 2);
         ctx.closePath();
         ctx.fill();
         ctx.stroke();
 
-        if (particle.size <= 0.1 && particle.opacity <= 0) {
+        if (particle.size <= 0.05 && particle.opacity <= 0) {
           particlesRef.current.splice(index, 1);
         }
       });
 
-      if (Math.random() > 0.66) {
+      if (Math.random() > 0.8) {
         createParticles();
       }
 
@@ -83,14 +83,14 @@ const ParticleCanvas: React.FC = () => {
     };
   }, []);
 
-  return <canvas ref={canvasRef} style={{ display: "block", backgroundColor: "black", margin: "auto" }} />;
+  return <canvas className="m-auto  box-border block " ref={canvasRef} />;
 };
 export const Particles = () => {
 
   return (
     <>
       <div
-        className="absolute flex items-center justify-center inset-0 z-0 border-4 border-white"  
+        className="absolute flex items-center justify-center pt-40 inset-0 z-0 border-4 border-white"
       >
         <ParticleCanvas />
       </div>
